f16a60221df7cffd421ed820be04208ba40e90fa
MarkS46 <you@example.com>
2022-02-14 13:29:58 +0100

    more antibiotics added

--- Main1AC.cpp
+++ Main1AC.cpp
@@ -5,35 +5,32 @@
 #include <cstdlib>
 #include <cmath> 
 
-//*** THE EFFECT OF WALL ATTACHMENT ON PLASMID PERSISTENCE SCRIPT 1 *********
+//*** THE EFFECT OF WALL ATTACHMENT ON PLASMID PERSISTENCE SCRIPT 3 *********
 
-// This script provides a simple time based simulation for the Stewart and Levin model
-// The parameters are currently set to have no persistence of the plasmid
-// Increasing the input resource concentraton to for example 25 will show persistence
-// In this script no extincition criteria is set for it was not used in the report visualization
-// that means that you will not see any concentrations going to 0 because this takes a long time
-// if at the end of the simulation however you will see a decreasing concentration that means it will most likely go extinct.
+// This script provides a way to record equillibrium values while varying a parameter value
+// the below found script produces the left graph in figure 1 in the report
+// But it was modified to create figure 1 and 2 
+// This script has an extinction criteria so that when the plasmid won't persist it's value 
+// will be set to 0 
 
 //*** model parameters *********
 
-    const double K0 = 4.0; // half saturation constant of plasmid free for resource
-    const double K1 = 4.0; // half saturation constant of plasmid bearing for resource
+    const double K0 = 4.0; // half saturation constant of plasmid bearing 
+    const double K1 = 4.0; // half saturation constant of plasmid bearing
     const double e1 = 6.25 * 1e-7; // resource needed to divide once for plasmid bearing cells
     const double e0 = 6.25 * 1e-7; // resource needed to divide once for plasmid free cells
     const double l = 1e-3; // loss of plasmid by divsion
     const double c = 1e-9; // conjugation rate
-    const double D = 0.25; // rate of turnover
-    const double r0 = 0.738; // growth rate of plasmid free
-    const double r1 = 0.6642; // growth rate of plasmid bearing
-    const double Sin = 16; // input resource concentration    
-    const double alfa = 1 - (r1/r0); // selective advantage of plasmid free cells
+    const double D = 0.25; // turnover rate
+    const double r0 = 0.738; // max growth rate of plasmid free
+    const double r1 = 0.6642; // max growth rate of plasmid bearing
+    double Sin;  // input resource concentration  
     const double d1 = 1e-9; // death of plasmid bearing cell by antibiotic
     const double d0 = 1e-2; // death of plasmid free cell by antibiotic
-    const double Ain = 0; // input antibiotic concentration
+    const double Ain = 0; // input antibiotic concentration 
     const double H0 = 4.0; // half saturation constant of plasmid free for antibiotics
     const double H1 = 4.0; // half saturation constant of plasmid bearing for antibiotics
     const double Adisp = 1e-5; // antibiotic needed for the death of a cell
-
 
 //*** ODE description *********
 
@@ -53,7 +50,7 @@ void rhs(const double &t, const std::vector<double> &x, std::vector<double> &dxd
     dxdt[1] = psi0 * N0  - D * N0 + l * N1 - c * N0 * N1 - die0 * N0; // differential equation of the plasmid free cells
     dxdt[2] = psi1 * N1 - D * N1 - l * N1 + c * N0 * N1 - die1 * N1; // differential equation of the plasmid bearing cells
     dxdt[3] = D * (Ain - A) - Adisp * die0 * N0 - Adisp * die1 * N1; // differential equation of the antibiotic 
-}
+ }
 
 //*** parameters of the integration algorithm *********
 
@@ -62,8 +59,7 @@ void rhs(const double &t, const std::vector<double> &x, std::vector<double> &dxd
     const double kdSafety = 0.9; // safety factor in adaptive stepsize control
     const double kdMinH = 1.0e-6; // minimum step size
     const int nvar = 4; // number of variables
-    const double dt0 = 0.001; // initial time step size
-    const double dtsav = 0.05; // save data after time steps
+    const double dt0 = 0.0005; // initial time step size
     const double tEnd = 100000.0; // end time
     const double tolerance = 1.0e-6; // acceptable local error during numerical integration
 
@@ -71,6 +67,7 @@ void rhs(const double &t, const std::vector<double> &x, std::vector<double> &dxd
 
 bool BogackiShampineStepper(double &t, std::vector<double> &x,  std::vector<double>&dxdt, double &h)
 {
+
     // step 2
     std::vector<double> xtmp(nvar);
     for(int i = 0; i < nvar; ++i)
@@ -144,91 +141,98 @@ bool BogackiShampineStepper(double &t, std::vector<double> &x,  std::vector<doub
     }
 }
 
- //*** function main() *********
+// analysis function to allow for easy looping over variables
+void do_analysis(std::string output_filename, const std::vector<double>& pars) 
+{
+    // give initial values 
+    double initialN0 = 1.0;
+    double initialN1 = 1.0e-5;
+
+    std::vector<double> x(nvar);
+    x[0] = Sin;
+    x[1] = initialN0;
+    x[2] = initialN1;
+    x[3] = Ain;
+    std::vector<double> dxdt(nvar);
+    rhs(0.0, x, dxdt);
+
+    // start numerical integration
+    int nOK = 0, nStep = 0;
+    for(double t = 0.0, dt = dt0; t < tEnd; ++nStep)
+    {
+        if(BogackiShampineStepper(t, x, dxdt, dt))
+        {
+            ++nOK;
+        }
+        if (fabs(dxdt[1]) < 1.0e-6 && fabs(dxdt[2]) < 1.0e-6) // if change very little, stop (equilibrium most likely reached)
+        {
+            break;
+        }
+    }
+
+
+    if (fabs(dxdt[2]) < 1.0e-6 && x[2] < initialN1 * 1000) // if the equillibrium value is reached and the value is not 1000
+    {                                                      // times larger than intial value, set it to 0 (cause it will go extinct) 
+        x[2] = 0;
+    }
+    if (fabs(dxdt[1]) < 1.0e-6 && x[1] < initialN0 * 1000)
+    {
+        x[1] = 0;
+    }
+
+    // open file
+    std::ofstream ofs(output_filename.c_str(), std::ios::app);
+    if(!ofs.is_open())
+    {
+        throw std::runtime_error("unable to open file.\n");
+    }
+
+    // write the paramater(s) followed by the concentration of the cell types
+    for(size_t i = 0; i < pars.size(); ++i )
+    {
+        ofs << pars[i] << ',';
+    } 
+    ofs << x[1] << ',' << "N0" << ',' << dxdt[1] << '\n';
+
+    for(size_t i = 0; i < pars.size(); ++i )
+    {
+        ofs << pars[i] << ',';
+    } 
+    ofs << x[2] << ',' << "N1"  << ',' << dxdt[2] << '\n';
+
+
+    ofs.close();
+}
+
+//*** function main() *********
 
 int main()
 {
     try 
-    {
-        // open data file
-        std::ofstream ofs("Basic1C2.csv");
-        if(!ofs.is_open())
-        {
-            throw std::runtime_error("unable to open file.\n");
-        }      
+    {   
+        // provide file name
+        std::string file_name = "Main1AC9.csv";
+        std::ofstream ofs(file_name.c_str());
 
         // give first row with variable names
-        ofs << "t" << ',' << "popsize" << ',' << "population" << ',' << "resource input" << "\n";
-
-        // give initial values
-        double initialN0 = 1.0;
-        double initialN1 = 1e-5;
-
-        std::vector<double> x(nvar);
-        x[0] = Sin;
-        x[1] = initialN0;
-        x[2] = initialN1;
-        x[3] = Ain; 
-
-        std::vector<double> dxdt(nvar);
-        rhs(0.0, x, dxdt);
-
-        // initiate parameters for the integration
-        int nOK = 0, nStep = 0;
-        double dtMin = dt0, dtMax = kdMinH; 
-
-        // start numerical integration
-        for(double t = 0.0, tsav = 0.0, dt = dt0; t < tEnd; ++nStep) 
-        {
-            if(BogackiShampineStepper(t, x, dxdt, dt))
-            {
-                ++nOK; // number of OK steps to be recorded
-            }
-            if (fabs(dxdt[1]) < 1.0e-6 && fabs(dxdt[2]) < 1.0e-6) // if change very little, stop (equilibrium most likely reached)
-            {
-                std::cout << "t = " << t << "\n";
-                break; 
-            } 
-            if(dt < dtMin) // keep track of the smallest step size
-            {
-                dtMin = dt;
-            }
-            else if(dt > dtMax) // keep track of the largest step size  
-            {
-                dtMax = dt;
-            }
-
-            if(t > tsav) // save the data every 0.05 time steps
-            {
-                ofs << t << ',' << x[1] << ',' << "N0" << ',' << Sin << '\n' 
-                    << t << ',' << x[2] << ',' << "N1" << ',' << Sin << '\n'; 
-                tsav += dtsav;
-            }
-        }
-
-        // report integration data
-        std::cout << "integration complete.\n"
-        << "number of steps : " << nStep << '\n'
-        << "proportion bad steps : " << 1.0 - nOK * 1.0 / nStep << '\n'
-        << "average step size : " << tEnd / nStep << '\n'
-        << "min step size : " << dtMin << '\n'
-        << "max step size : " << dtMax << "\n\n";
-
-        // return population concentrations at end of simulation
-        std::cout << "plasmid free = " << x[1] << " plasmid bearing = " << x[2] <<"\n" << "resource = " << x[0] << " Antibiotics = " << x[3] <<"\n";
-
-        // return alpha
-        std::cout << "alpha = "  << alfa << "\n";
-
-        // return concencration of plasmid bearing cells
-        std::cout << "F+ = " << (x[2] / (x[1] + x[2]))*100 << "\n"; 
-
+        ofs  << "pars" << ',' << "popsize" << ',' << "population" << ',' << "DXDT" << "\n";
         ofs.close();
+
+        // do analysis for different values of the resource input concentration 
+        for (double local_Sin = 5 ; local_Sin < 100; ++local_Sin ) 
+        {
+            Sin = local_Sin;
+            std::vector<double> pars = {local_Sin};
+            do_analysis(file_name, pars);
+            std::cout << Sin << "\n";
+        }
     }
-    catch(std::exception &error) 
+
+    catch(std::exception &error)
     {
     std::cerr << "error: " << error.what();
     exit(EXIT_FAILURE);
     }
-    return 0; 
+   
+    return 0;
 }
--- Main1C.R
+++ Main1C.R
@@ -1,19 +1,19 @@
 library (ggplot2)
 library(readr)
 
-data <-  Main1C
+data <- Main1AC9
 
 ggplot(data=data, aes(x=pars)) +
   geom_point(aes(y = popsize, color = population), size = 1.5, alpha = 1) +
   geom_line(aes(y = popsize, color = population), size = 1) +
   scale_y_continuous(trans = "log10") +
   scale_x_continuous(trans = "log10") +
-  annotate("text", x=68, y=0.0001, label= "D = 0.25" , size = 8) +
+  annotate("text", x=57, y=0.0001, label= "Ain = 0" , size = 8) +
 
 
   labs(title = "The effect of resource input concentration \non equilibrium population sizes", 
        y = "Concentration of bacteria per ml",
-       x = "resource input concentration (Sin)")  + 
+       x = "Resource input concentration (Sin)")  + 
   
   theme(
     plot.title = element_text(size = 18),
