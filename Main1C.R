0bd40c19e72718021a82df6512b75f6513b6ebd1
MarkS46 <you@example.com>
2022-02-15 15:32:49 +0100

    adding antib in 2c

--- Main1C.R
+++ Main1C.R
@@ -1,19 +1,19 @@
 library (ggplot2)
 library(readr)
 
-data <- Main1ACH2
+data <- Main1ACAdisp2
 
 ggplot(data=data, aes(x=pars)) +
   geom_point(aes(y = popsize, color = population), size = 1.5, alpha = 1) +
   geom_line(aes(y = popsize, color = population), size = 1) +
   scale_y_continuous(trans = "log10") +
   scale_x_continuous(trans = "log10") +
-  annotate("text", x=5, y=1, label= "Ain = 20\nSin = 16\nd0/1 = 1e-2\nH0 = 4" , size = 5) +
+  annotate("text", x=2, y= 0.001, label= "Adisp1 = 1e-4" , size = 5) +
 
 
-  labs(title = "The effect of H1 \non equilibrium population sizes", 
+  labs(title = "The effect of Ain \non equilibrium population sizes", 
        y = "Concentration of bacteria per ml",
-       x = "half saturation constant H1 (plasmid bearing)")  + 
+       x = "antibiotic input concentratoin (Ain)")  + 
   
   theme(
     plot.title = element_text(size = 18),
